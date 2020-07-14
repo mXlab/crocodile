8dea764e9c313218de1e73fa4338d3c2c69443d0
Montenegro93 <43181133+Montenegro93@users.noreply.github.com>
2020-07-14 13:07:46 -0400

    Added marker system, added osc communication and cleaned the code

--- arduino_Interval_timer_1000hz_w_sd/Global.h
+++ arduino_Interval_timer_1000hz_w_sd/Global.h
@@ -1,8 +1,14 @@
+
 /////////////////////////////////GLOBAL VARIABLES//////////////////////////////////
+/*
+    This file contains all the #include statements, all the define statements
+    and all the global variable definition
 
+*/
 
-//include Custom recording class
-#include "Recording.h"
+//----------------------------------ADDING LIBRARIES-------------------------------//
+
+#include "Recording.h" //include Custom recording class
 
 
 //include sd library
@@ -10,6 +16,7 @@
 #include <SD_t3.h>
 #include <SPI.h>
 
+//include Ethernet library
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
@@ -17,25 +24,43 @@
 //include biodata library
 //some of these file could be removed
 #include <Respiration.h>
-#include <MinMax.h>
-#include <Lop.h>
-#include <Threshold.h>
-//#include <Hip.h>
+#include <MinMax.h> //this one
+#include <Lop.h> //this one
+#include <Threshold.h> //this one
+//#include <Hip.h> //this file is causing a bug
 #include <SkinConductance.h>
-#include <Average.h>
+#include <Average.h> // this one 
 #include <Heart.h>
 
 
 //include circular buffer library
 #include <CircularBuffer.h>
 
+//include OSC library
+//could probaly just keep a couple of these
+#include <OSCBoards.h>
+#include <OSCBundle.h>
+#include <OSCData.h>
+#include <OSCMatch.h>
+#include <OSCMessage.h>
+#include <OSCTiming.h>
+#include <SLIPEncodedSerial.h>
+#include <SLIPEncodedUSBSerial.h>
+
+
+//----------------------------------DEFINE STATEMENTS-------------------------------//
+
 #define CS_PIN 10
-#define BUFFER_SIZE 512 //set the buffer size here 
+#define BUFFER_SIZE 640 //set the buffer size here. it needs the be a multiple of the number of columns ex: timestamps, marker, heart, gsr, resp -- 5 columns so the buffer is 640
 #define NUM_SIGNALS 3  //Set number of signal recorded here 
+
+
+//-----------------------------GLOBAL VARIABLES DEFINITION--------------------------//
 
 
 IntervalTimer captureData; //create instance of Interval timer for sensors
 Recording r(NUM_SIGNALS); //create instance of Recording object
+
 
 Heart heart(A0); //Create instance for heart sensor
 SkinConductance sc1(A3); //Create instance for gsr sensor
@@ -47,37 +72,37 @@ Respiration resp(A2); //create instance for respiration sensor
 Sd2Card card;
 SdVolume volume;
 SdFile root;
-File recFile;
-bool fileOpen = false;
-const int chipSelect = 4; //cs for sd card
+
+File recFile; //create a instance of File object for the recording file
+bool fileOpen = false; //boolean to keep track of if a file is open to write
+const int chipSelect = 4; //cs pin for sd card
+char filename[11] = "rec010.txt";  //initial filename here
 
 
 
-char filename[11] = "rec050.txt";  //initial fileanme here
-
-
-//Setup UDP
-
-
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-IPAddress ip(169,254,2,78);
+// Enter da MAC address and IP address for your controller below.
+// The IP adress will be dependent on your local network:
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //default Arduino mac adress
+IPAddress ip(169,254,2,78); //Microcontroller ip adress
 
 unsigned int localPort = 8888;      // local port to listen on
+int computerPort = 8888; //check if can replace this for localPort in the code ( most probably yes ) because they hold the same value
+IPAddress computerIP ; //ip adress of the computer we're talking to. 
 
-IPAddress computerIP ;
-int computerPort ;
 // buffers for receiving and sending data
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet,
-char ReplyBuffer[] = "aCknowledged";        // a string to send back
+// can probably remove these two buffer because using osc now
+//char packetBuffer[UDP_TX_PACKET_MAX_SIZE];  // buffer to hold incoming packet,
+//char ReplyBuffer[] = "aCknowledged";        // a string to send back
 
-// An EthernetUDP instance to let us send and receive packets over UDP
-EthernetUDP Udp;
+//OSCBundle sample; //create an osc bundle object to send the sample @ 1000hz
+EthernetUDP Udp; // An EthernetUDP instance to let us send and receive packets over UDP
 
+
+CircularBuffer<unsigned long , BUFFER_SIZE >bufferA; //Create an instance of a circular buffer to store data before sending
+unsigned long writeBuffer[BUFFER_SIZE] = {0}; // Temporary buffer that holds the data while writting to card
+
+
+//These bools can be used as a protection to prevent starting a recording without all th infos for the header
 bool Connected = false; 
 bool nameReceived = false; 
 bool locReceived = false; 
@@ -85,12 +110,6 @@ bool dateReceived = false;
 bool rateReceived = false;
 
 
+bool readyToWrite = false; //boolean to prevent writing to the card when its not time
 
-
-
-
-//Create instances for circular buffers
-CircularBuffer<unsigned long , BUFFER_SIZE >bufferA;
-unsigned long writeBuffer[BUFFER_SIZE] = {0};
-
-bool readyToWrite = false;
+unsigned long stamp = 0; //holds the millis timestamp when the recording starts
