79c14e6ba340acde191a8920c550ba8570f9f3fa
Hugo Berard <berard.hugo@gmail.com>
2020-09-11 12:33:13 -0400

    Adding new architectures for GAN model

--- train.py
+++ train.py
@@ -19,7 +19,8 @@ from lib.fid import FID
 
 def get_config():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', default="small", choices=("small",))
+    parser.add_argument('-m', '--model', default="small", choices=("small", "resnet"))
+    parser.add_argument('--mode', default="hinge")
     parser.add_argument('-e', '--num-epochs', default=1000, type=int)
     parser.add_argument('-r', '--resolution', default=128, type=int)
     parser.add_argument('-f', '--num-filters', default=128, type=int)
@@ -35,11 +36,12 @@ def get_config():
     parser.add_argument('--eval-freq', default=1, type=int)
     parser.add_argument('--path-to-dataset', default="/network/tmp1/berardhu/crocodile/data", type=str)
     parser.add_argument('--output-path', default="/network/tmp1/berardhu/crocodile/results/", type=str)
+    parser.add_argument('--compute-fid', action="store_true")
     args = parser.parse_args()
     return args
 
 
-def run(args, logger=None):  
+def run(args, logger=None):
     EMA = args.ema
     BATCH_SIZE = args.batch_size
     NUM_Z = args.num_latent
@@ -64,17 +66,24 @@ def run(args, logger=None):
 
     print("Init...")
 
-    path_to_stats = os.path.join(args.path_to_dataset, str(args.resolution), "crocodile_stats.pt")
-    if not os.path.exists(path_to_stats):
-        print("Computing stats for dataset...")
-        mu, sigma = FID(device=device).compute_stats(dataloader)
-        torch.save(dict(mu=mu, sigma=sigma), path_to_stats)
+    if args.compute_fid:
+        path_to_stats = os.path.join(args.path_to_dataset, str(args.resolution), "crocodile_stats.pt")
+        if not os.path.exists(path_to_stats):
+            print("Computing stats for dataset...")
+            mu, sigma = FID(device=device).compute_stats(dataloader)
+            torch.save(dict(mu=mu, sigma=sigma), path_to_stats)
 
-    fid_score = FID(path_to_stats, device=device)
+        fid_score = FID(path_to_stats, device=device)
 
     if args.model == "small":
-        gen = models.SmallGenerator(NUM_Z, RESOLUTION, NUM_FILTERS, args.num_layers, spectral_norm=args.spectral_norm_gen).to(device)
-        dis = models.SmallDiscriminator(RESOLUTION, NUM_FILTERS, args.num_layers).to(device)
+        gen = models.SmallGenerator(NUM_Z, RESOLUTION, NUM_FILTERS, args.num_layers, spectral_norm=args.spectral_norm_gen)
+        dis = models.SmallDiscriminator(RESOLUTION, NUM_FILTERS, args.num_layers)
+    elif args.model == "resnet":
+        gen = models.ResNetGenerator(NUM_Z, RESOLUTION, NUM_FILTERS, args.num_layers)
+        dis = models.ResNetDiscriminator(RESOLUTION, NUM_FILTERS, args.num_layers)
+
+    gen = gen.to(device)
+    dis = dis.to(device)
 
     gen_optimizer = optim.Adam(gen.parameters(), lr=LR_GEN, betas=(0.5, 0.999))
     dis_optimizer = optim.Adam(dis.parameters(), lr=LR_DIS, betas=(0.5, 0.999))
@@ -103,7 +112,7 @@ def run(args, logger=None):
             x_gen = gen.sample(len(x))
 
             score_true, score_gen = dis(x), dis(x_gen)
-            loss_gen, loss_dis = utils.compute_loss(score_true, score_gen, mode="nsgan")
+            loss_gen, loss_dis = utils.compute_loss(score_true, score_gen, mode=args.mode)
             if GRADIENT_PENALTY:
                 loss_dis += GRADIENT_PENALTY*dis.get_penalty(x, x_gen)
 
@@ -122,7 +131,9 @@ def run(args, logger=None):
         x_gen = gen(z_examples)
         x_gen = x_gen/2 + 0.5
 
-        fid = fid_score(gen)
+        fid = 0
+        if args.compute_fid:
+            fid = fid_score(gen)
 
         if logger is None:
             print("Epoch: %i, Loss dis: %.2e, Loss gen %.2e, FID: %.2e, Time: %i"%(init_epoch+epoch, loss_dis, loss_gen, fid, time.time()-t))
