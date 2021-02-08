aa58e985dab9c3b407a9f26dd970562032f6bddc
Erin Gee <erin.marie.gee@gmail.com>
2021-02-08 15:27:42 -0500

    Cosmetic updates
    cleaned up comment sections

--- BioSynth_arduino_code/Crocodile_BOX_Prototype_INTEGRATION/Global.h
+++ BioSynth_arduino_code/Crocodile_BOX_Prototype_INTEGRATION/Global.h
@@ -7,7 +7,7 @@
 
 //----------------------------------DEFINE STATEMENTS-------------------------------//
 
-#define CS_PIN 10   // can this be rerouted to a non PWM pin?
+#define CS_PIN 10   
 #define BUFFER_SIZE 768 //set the buffer size here. it needs the be a multiple of the number of columns ex: timestamps, marker, heart, gsr, resp -- 5 columns so the buffer is 640
 #define NUM_SIGNALS 5  //Set number of signal recorded here 
 
@@ -70,9 +70,9 @@ IntervalTimer captureData; //create instance of Interval timer for sensors
 Recording r(NUM_SIGNALS); //create instance of Recording object
 
 
-Heart heart(A7); //Create instance for heart sensor
-SkinConductance sc1(A2); //Create instance for gsr sensor
-SkinConductance sc2(A6);
+Heart heart(A7); // create instance for heart sensor
+SkinConductance sc1(A2); // create instance for gsr sensor1
+SkinConductance sc2(A6); // create instance for gsr sensor2
 Respiration resp(A3); //create instance for respiration sensor
 
 // set up variables using the SD utility library functions:
@@ -80,9 +80,9 @@ Sd2Card card;
 SdVolume volume;
 SdFile root;
 
-File recFile; //create a instance of File object for the recording file
-bool fileOpen = false; //boolean to keep track of if a file is open to write
-const int chipSelect = 4; //cs pin for sd card
+File recFile; // create an instance of File object for the recording file
+bool fileOpen = false; // boolean to keep track of if a file is open to write
+const int chipSelect = 4; // cs pin for sd card
 
 String fileDigits = "000";
 String fileExtension = ".txt";
@@ -93,21 +93,20 @@ String signalTypes[NUM_SIGNALS] = {"Heart" , "GSR1","GSR2", "TEMP"};
 
 // Enter da MAC address and IP address for your controller below.
 // The IP adress will be dependent on your local network:
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //default Arduino mac adress
-IPAddress ip(169, 254, 2, 78); //Microcontroller ip adress
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // default Arduino mac adress
+IPAddress ip(169, 254, 2, 78); // Microcontroller ip adress
 
 unsigned int localPort = 8888;      // local port to listen on
 int computerPort = 8888; //check if can replace this for localPort in the code ( most probably yes ) because they hold the same value
-IPAddress computerIP ; //ip adress of the computer we're talking to.
+IPAddress computerIP ; // ip adress of the computer we're talking to
 
-//OSCBundle sample; //create an osc bundle object to send the sample @ 1000hz
-EthernetUDP Udp; // An EthernetUDP instance to let us send and receive packets over UDP
+//OSCBundle sample; // create an osc bundle object to send the sample @ 1000hz
+EthernetUDP Udp; // an EthernetUDP instance to let us send and receive packets over UDP
 
 CircularBuffer<unsigned long , BUFFER_SIZE >bufferA; //Create an instance of a circular buffer to store data before sending
-unsigned long writeBuffer[BUFFER_SIZE] = {0}; // Temporary buffer that holds the data while writting to card
+unsigned long writeBuffer[BUFFER_SIZE] = {0}; // Temporary buffer that holds the data while writing to card
 
-
-//These bools can be used as a protection to prevent starting a recording without all th infos for the header
+//These bools can be used as protection to prevent starting a recording without all the infos for the header
 bool Connected = false;
 bool nameReceived = false;
 bool locReceived = false;
@@ -115,13 +114,11 @@ bool dateReceived = false;
 bool rateReceived = false;
 
 
-bool readyToWrite = false; //boolean to prevent writing to the card when its not time
+bool readyToWrite = false; // boolean to prevent writing to the card when it's not time
 
-unsigned long stamp = 0; //holds the millis timestamp when the recording starts
+unsigned long stamp = 0; // holds the millis timestamp when the recording starts
 
 bool filenameAvailable = false;
-
-
 
 Bounce startButton =  Bounce();
 Bounce markerButton = Bounce();
