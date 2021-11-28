b453e102a846176fba2bae5ec7423e4cc8f156c3
Hugo Berard <hberard@fb.com>
2021-11-28 17:28:32 -0500

    fixing bug

--- pygan/scripts/train.py
+++ pygan/scripts/train.py
@@ -24,16 +24,17 @@ class Train(Launcher):
         print("Loading dataset...")
         dataset = LaurenceDataset(args.dataset)
 
+        data_path = dataset.get_path()
+
         if args.train.model_type == ModelType.STYLEFORMER:
-            data_path = dataset.get_path()
+            output_dir = args.train.output_dir.resolve()
             gpus = torch.cuda.device_count()
             os.chdir('pygan/models/Styleformer')
             command = "python train.py --outdir=%s --data=%s --gpus=%i --num_layers=1,2,1,1 --g_dict=1024,256,64,64 --linformer=1" % (
-                args.train.output_dir.resolve(), data_path, gpus)
+                output_dir, data_path, gpus)
             print("Running: %s" % command)
             subprocess.run(command.split())
         elif args.train.model_type == ModelType.FASTGAN:
-            data_path = dataset.get_path()
             os.chdir('pygan/models/FastGAN')
             command = "python train.py --path=%s --batch_size %i --im_size %i" % (
                 data_path, args.train.batch_size, dataset.resolution)
