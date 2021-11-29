222dad788da8c9d225207ec5b1c79e970b8910f4
Hugo Berard <hberard@fb.com>
2021-11-29 10:47:58 -0500

    Updating generate.py

--- pygan/scripts/generate.py
+++ pygan/scripts/generate.py
@@ -14,8 +14,14 @@ class Generate(Launcher):
         model_path: Path = None
         num_frames: int = 100
         model_type: ModelType = ModelType.STYLEFORMER
-        output_dir: Path = Path("./results/styleformer/test1")
+        output_dir: Path = None
         output_size: int = None
+        name: str = "test1"
+
+        def __post_init__(self):
+            if self.output_dir is None:
+                self.output_dir = Path("./results/%s/%s" % (
+                                       self.model_type.value, self.name))
 
     def __init__(self, args):
         super().__init__(args)
@@ -37,7 +43,7 @@ class Generate(Launcher):
                 output_dir, model_path, args.num_frames)
             print("Running: %s" % command)
             subprocess.run(command.split())
-            
+
         elif args.model_type == ModelType.FASTGAN:
             os.chdir('pygan/models/FastGAN')
             command = "python eval.py %s --ckpt=%s --im_size %i --out_size %i --n_samples %i" % (
