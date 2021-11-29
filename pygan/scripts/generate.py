941d11860e99b4ebe929959295286f6163d91577
Hugo Berard <hberard@fb.com>
2021-11-29 10:39:34 -0500

    Adding FastGAN to generation script

--- pygan/scripts/generate.py
+++ pygan/scripts/generate.py
@@ -1,6 +1,7 @@
 from pygan.launcher import Launcher
-from dataclasses import dataclass
 from pygan.models import ModelType
+from pygan.dataset import LaurenceDataset
+from dataclasses import dataclass
 from pathlib import Path
 import os
 import subprocess
@@ -14,11 +15,17 @@ class Generate(Launcher):
         num_frames: int = 100
         model_type: ModelType = ModelType.STYLEFORMER
         output_dir: Path = Path("./results/styleformer/test1")
+        output_size: int = None
 
     def __init__(self, args):
         super().__init__(args)
 
     def run(self, args):
+        dataset = LaurenceDataset(args.dataset)
+
+        if args.output_size is None:
+            args.ouput_size = dataset.resolution
+
         model_path = args.model_path.resolve()
         output_dir = args.output_dir.resolve()
         if model_path is None or not model_path.is_file():
@@ -28,6 +35,13 @@ class Generate(Launcher):
             os.chdir('pygan/models/Styleformer')
             command = "python generate.py --outdir=%s --network %s --num_frames %i" % (
                 output_dir, model_path, args.num_frames)
+            print("Running: %s" % command)
+            subprocess.run(command.split())
+            
+        elif args.model_type == ModelType.FASTGAN:
+            os.chdir('pygan/models/FastGAN')
+            command = "python eval.py %s --ckpt=%s --im_size %i --out_size %i --n_samples %i" % (
+                output_dir, model_path, dataset.resolution, args.output_size, args.num_frames)
             print("Running: %s" % command)
             subprocess.run(command.split())
 
