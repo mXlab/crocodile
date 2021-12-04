f13fac4cd7698243d9d18c47f540565412ea1324
Hugo Berard <hberard@fb.com>
2021-12-04 09:41:46 -0500

    updating generate script

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,6 +1,5 @@
-from crocodile.launcher import Launcher
-from crocodile.generator import ModelType
-from crocodile.dataset import LaurenceDataset
+from crocodile.executor import load_executor, ExecutorConfig
+from crocodile.generator import load_from_path
 from dataclasses import dataclass
 from pathlib import Path
 import torch
@@ -11,50 +10,46 @@ import os
 from torchvision import utils as vutils
 
 
-class Generate(Launcher):
-    @dataclass
-    class Params(Launcher.Params):
-        model_path: Path = None
-        num_frames: int = 100
-        batch_size: int = 16
-        model_type: ModelType = ModelType.STYLEFORMER
-        output_dir: Optional[Path] = None
-        name: str = "test1"
+@dataclass
+class EvalParams:
+    generator_path: Path
+    num_frames: int = 100
+    batch_size: int = 16
+    epoch: Optional[int] = None
+    output_dir: Optional[int] = None
 
-        def __post_init__(self):
-            if self.output_dir is None:
-                self.output_dir = Path("./results/%s/%s" % (
-                                       self.model_type.value, self.name))
+    def __post_init__(self):
+        if self.output_dir is None:
+            self.output_dir = self.generator_path / "eval"
 
-    def run(self, args):
-        model_path = args.eval.model_path.resolve()
-        output_dir = args.eval.output_dir.resolve()
 
-        device = torch.device('cuda')
+def run(self, args: EvalParams = EvalParams()):
+    device = torch.device('cuda')
+    generator = load_from_path(args.generator_path, args.epoch, device=device)
 
-        model = ModelType.load(args.eval.model_type)
-        model = model.load_model(model_path, device=device)
+    start_noise = generator.sample_z()
+    end_noise = generator.sample_z()
+    alpha = torch.linspace(
+        0, 1, args.num_frames).to(device).view(-1, 1)
+    noise = alpha*start_noise + (1-alpha)*end_noise
 
-        start_noise = model.sample_z()
-        end_noise = model.sample_z()
-        alpha = torch.linspace(
-            0, 1, args.eval.num_frames).to(device).view(-1, 1)
-        noise = alpha*start_noise + (1-alpha)*end_noise
+    args.output_dir.mkdir(exist_ok=True)
 
-        with torch.no_grad():
-            for i in tqdm(range(args.eval.num_frames//args.eval.batch_size + 1)):
-                _noise = noise[i*args.batch: (i+1)*args.eval.batch_size]
-                img = model(_noise)[0]
-                for j, g_img in enumerate(img):
-                    vutils.save_image(g_img.add(1).mul(0.5),
-                                      os.path.join(output_dir, f'{i*args.eval.batch_size+j:04d}.png'))
+    with torch.no_grad():
+        for i in tqdm(range(args.num_frames//args.batch_size + 1)):
+            _noise = noise[i*args.batch_size: (i+1)*args.batch_size]
+            img = generator(_noise)[0]
+            for j, g_img in enumerate(img):
+                vutils.save_image(g_img.add(1).mul(0.5),
+                                  os.path.join(args.output_dir, f'{i*args.batch_size+j:04d}.png'))
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_arguments(LaurenceDataset.Params, dest="dataset")
-    parser.add_arguments(Generate.Params, dest="eval")
+    parser.add_arguments(ExecutorConfig, dest="executor")
+    parser.add_arguments(EvalParams, dest="eval")
     args = parser.parse_args()
 
-    launcher = Generate(args.eval)
-    launcher.launch(args)
+    executor = load_executor(args.executor)
+
+    executor(run, args.eval)
