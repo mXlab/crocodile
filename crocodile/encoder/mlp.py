e9774cfa900ba0cd21a891a615a746348d73fba3
Hugo Berard <hberard@fb.com>
2021-12-05 17:55:13 -0500

    fixing bug

--- crocodile/encoder/mlp.py
+++ crocodile/encoder/mlp.py
@@ -12,6 +12,6 @@ class MLP(Encoder):
     def forward(self, x):
         return self.network(x.view(len(x), -1))
 
-    def buidl(self, input_dim: int, output_dim: int, device=None):
+    def build(self, input_dim: int, output_dim: int, device=None):
         self.network = nn.Sequential(nn.Linear(input_dim, self.params.num_hidden), nn.ReLU(
         ), nn.Linear(self.params.num_hidden, output_dim))
