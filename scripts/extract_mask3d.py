324900102b602beba497c8acc5bf6239040a9b4e
Hugo Berard <hberard@fb.com>
2021-12-05 16:55:59 -0500

    updating face3d

--- scripts/extract_mask3d.py
+++ scripts/extract_mask3d.py
@@ -1,4 +1,4 @@
-from crocodile.launcher import Launcher
+from crocodile.executor import load_executor, ExecutorConfig
 from dataclasses import dataclass
 from crocodile.face3d import Face3dModel
 from pathlib import Path
@@ -7,29 +7,30 @@ import subprocess
 from simple_parsing import ArgumentParser
 
 
-class Face3D(Launcher):
-    @dataclass
-    class Params(Launcher.Params):
-        path: Path = Path("./results/fastgan/test_1/eval")
-        model_type: Face3dModel = Face3dModel.DDFA_V2
+@dataclass
+class Params:
+    images_path: Path = Path("./results/fastgan/test_1/eval")
+    model_type: Face3dModel = Face3dModel.DDFA_V2
 
-    def run(self, args):
-        path = args.path.resolve()
-        if path is None or not path.is_dir():
-            raise("Please specify a valid path for the directory containing the images.")
 
-        if args.model_type == Face3dModel.DDFA_V2:
-            os.chdir('pygan/face3d/3DDFA_V2')
-            command = "python demo.py -f %s -o obj" % (
-                path)
-            print("Running: %s" % command)
-            subprocess.run(command.split())
+def run(self, args):
+    path = args.images_path.resolve()
+    if path is None or not path.is_dir():
+        raise("Please specify a valid path for the directory containing the images.")
+
+    if args.model_type == Face3dModel.DDFA_V2:
+        os.chdir('pygan/face3d/3DDFA_V2')
+        command = "python demo.py -f %s -o obj" % (
+            path)
+        print("Running: %s" % command)
+        subprocess.run(command.split())
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_arguments(Face3D.Params, dest="train")
+    parser.add_arguments(ExecutorConfig, dest="executor")
+    parser.add_arguments(Params, dest="params")
     args = parser.parse_args()
 
-    launcher = Face3D(args.train)
-    launcher.launch(args.train)
+    executor = load_executor(args.executor)
+    executor(run, args.params)
