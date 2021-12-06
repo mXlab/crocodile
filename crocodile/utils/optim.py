a3ba6c73f89aad1432b1b734e2e208060febce6b
Hugo Berard <hberard@fb.com>
2021-12-06 17:53:26 -0500

    fixing bug

--- crocodile/utils/optim.py
+++ crocodile/utils/optim.py
@@ -4,6 +4,7 @@ from simple_parsing.helpers import Serializable
 from simple_parsing.helpers.serialization import encode, register_decoding_fn
 import torch.optim as optim
 from typing import Optional
+import torch
 
 
 class OptimizerType(Enum):
@@ -15,6 +16,7 @@ class OptimizerType(Enum):
 class OptimizerArgs(Serializable):
     optimizer: OptimizerType = OptimizerType.SGD
     lr: Optional[float] = None
+    noise_scale: Optional[float] = None
     
     def __post_init__(self):
         if self.lr is None:
@@ -37,8 +39,29 @@ def decode_optimizer_type(name: str) -> OptimizerType:
 register_decoding_fn(OptimizerType, decode_optimizer_type)
 
 
-def load_optimizer(parameters, args: OptimizerArgs = OptimizerArgs()):
+def load_optimizer(parameters, args: OptimizerArgs = OptimizerArgs()) -> optim.Optimizer:
     if args.optimizer == OptimizerType.SGD:
-        return optim.SGD(parameters, lr=args.lr)
+        optimizer = optim.SGD(parameters, lr=args.lr)
     elif args.optimizer == OptimizerType.ADAM:
-        return optim.Adam(parameters, lr=args.lr)
+        optimizer = optim.Adam(parameters, lr=args.lr)
+
+    if args.noise_scale is not None:
+        optimizer = Langevin(optimizer, noise_scale=args.noise_scale)
+    return optimizer
+
+
+class Langevin(optim.Optimizer):
+    def __init__(self, optimizer: optim.Optimizer, noise_scale: float = 1.):
+        self.optimizer = optimizer
+        self.noise_scale = noise_scale
+
+    def zero_grad(self, set_to_none: bool = False):
+        self.optimizer.zero_grad()
+
+    def step(self, closure):
+        self.optimizer.step()
+        for group in self.optimizer.param_groups:
+            for p in group['params']:
+                noise = torch.zeros_like(p).normal_()
+                p.add_(noise, alpha=self.noise_scale)
+
