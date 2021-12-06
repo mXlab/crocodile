0b3007e9bded857df2c1b22fedc5ce964d06ab2d
Hugo Berard <hberard@fb.com>
2021-12-06 16:53:48 -0500

    fixing bug

--- crocodile/dataset/latent.py
+++ crocodile/dataset/latent.py
@@ -1,17 +1,18 @@
 import torch
-from typing import Optional, Callable, List
+from typing import Optional, Callable
 from pathlib import Path
+import torch.nn as nn
 
 
-class LatentDataset:
+class LatentDataset(nn.Module):
     def __init__(self, n: int, dim: Optional[int] = None, init_func: Optional[Callable[[int], torch.Tensor]] = None):
+        super().__init__()
         if init_func is None:
             self.latent = torch.zeros(n, dim)
         else:
             self.latent = init_func(n).cpu()
 
-    def parameters(self) -> List[torch.Tensor]:
-        return [self.latent]
+        self.latent = nn.Parameter(self.latent)
 
     def __getitem__(self, index: int) -> torch.Tensor:
         return self.latent[index]
