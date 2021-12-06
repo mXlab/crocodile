e95a4513cf046fb1c9f45fbfdf498ba38b73c8a1
Hugo Berard <hberard@fb.com>
2021-12-06 16:19:58 -0500

    fixing bug

--- scripts/compute_latent.py
+++ scripts/compute_latent.py
@@ -7,6 +7,7 @@ from crocodile.utils.logger import Logger
 from dataclasses import dataclass
 from torchvision import transforms
 from torch.utils.data.dataloader import DataLoader
+from torch.utils.data import Subset
 from pathlib import Path
 import torch
 from simple_parsing import ArgumentParser
@@ -30,7 +31,7 @@ class Params(Serializable):
     name: str = "test_1"
     num_test_samples: int = 10
     slurm_job_id: Optional[str] = os.environ.get('SLURM_JOB_ID')
-    im_size: int = 64
+    subset_size: Optional[int] = None
 
     def __post_init__(self):
         self.save_dir = self.log_dir / self.name
@@ -44,9 +45,7 @@ class ComputeLatent(ExecutorCallable):
         torch.manual_seed(1234)
         device = torch.device('cuda')
 
-        resize = transforms.Resize((args.im_size, args.im_size))
         transform_list = [
-            resize,
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
         ]
@@ -54,9 +53,14 @@ class ComputeLatent(ExecutorCallable):
 
         generator = load_from_path(
             args.generator_path, args.epoch, device=device)
+        args.dataset.resolution = generator.resolution
 
         dataset = LaurenceDataset(
             args.dataset, transform=trans, target_transform=transforms.ToTensor())
+
+        if args.subset_size is not None:
+            dataset = Subset(dataset, torch.randperm(
+                len(dataset))[:args.subset_size])
 
         dataloader = DataLoader(
             dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
@@ -65,15 +69,16 @@ class ComputeLatent(ExecutorCallable):
 
         optimizer = load_optimizer(latent_dataset.parameters(), args.optimizer)
 
-        loss_fn = load_loss(args.loss)
+        loss_fn = load_loss(args.loss.loss, args.loss)
+        loss_eval = load_loss()
 
         logger = Logger(args.save_dir)
         logger.save_args(args)
 
-        img, _, index_ref = iter(dataloader).next()
-        img = img[:args.num_test_samples]
+        img_ref, _, index_ref = iter(dataloader).next()
+        img_ref = img_ref[:args.num_test_samples]
         index_ref = index_ref[:args.num_test_samples]
-        logger.save_image("groundtruth", img)
+        logger.save_image("groundtruth", img_ref)
 
         for epoch in range(args.num_epochs):
             loss_mean = 0
@@ -85,7 +90,7 @@ class ComputeLatent(ExecutorCallable):
                 z = z.to(device)
                 z.requires_grad_()
 
-                img_recons = resize(generator(z))
+                img_recons = generator(z)
 
                 loss = loss_fn(img, img_recons)
                 loss.backward()
@@ -94,14 +99,15 @@ class ComputeLatent(ExecutorCallable):
 
                 loss_mean += loss.detach().item()
 
-            logger.add({"epoch": epoch, "loss_mean": loss_mean})
-
             with torch.no_grad():
                 z = latent_dataset[index_ref].to(device)
                 img_recons = generator(z)
+                loss = loss_eval(img_ref, img_recons).detach().item()
 
             logger.save_image(f"recons_{epoch:04d}", img_recons)
             logger.save("latent", latent_dataset)
+            logger.add(
+                {"epoch": epoch, "train_loss": loss_mean, "eval_loss": loss})
 
 
 if __name__ == "__main__":
