cd2c2697fe649ec2aafe2e117c7bc27179b046d2
Hugo Berard <hberard@fb.com>
2021-12-06 00:13:51 -0500

    Fixing bugs

--- crocodile/dataset/latent.py
+++ crocodile/dataset/latent.py
@@ -5,7 +5,8 @@ class LatentDataset:
     def __init__(self, n, dim=None, init_func=None):
         if init_func is None:
             self.latent = torch.zeros(n, dim)
-        self.latent = init_func(n).cpu()
+        else:
+            self.latent = init_func(n).cpu()
 
     def __getitem__(self, index):
         return self.latent[index]
