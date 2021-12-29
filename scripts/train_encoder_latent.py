fbbbf7e861e6ff788dcd11f0320871479e3f2ab7
Hugo Berard <berard.hugo@gmail.com>
2021-12-29 17:52:15 +0100

    Adding script to train encoder with latent

---
+++ scripts/train_encoder_latent.py
@@ -0,0 +1,85 @@
+from crocodile.executor import load_executor, ExecutorConfig, ExecutorCallable
+from crocodile.encoder import load_encoder
+from crocodile.dataset import LaurenceDataset, LatentDataset
+from crocodile.utils.optim import load_optimizer
+from crocodile.utils.loss import load_loss
+from crocodile.utils.logger import Logger
+from torchvision import transforms
+from torch.utils.data.dataloader import DataLoader
+import torch
+from simple_parsing import ArgumentParser
+from tqdm import tqdm
+from crocodile.params import TrainEncoderLatentParams as Params
+import os
+
+
+class TrainEncoder(ExecutorCallable):
+    def __call__(self, args: Params, resume=False):
+        args.slurm_job_id = os.environ.get('SLURM_JOB_ID')
+        device = torch.device('cuda')
+
+        transform_list = [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ]
+        trans = transforms.Compose(transform_list)
+
+        dataset = LaurenceDataset(
+            args.dataset, transform=trans, target_transform=transforms.ToTensor())
+        
+        dataloader = DataLoader(
+            dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+        latent_dataset = LatentDataset.load(args.latent_path)
+
+        loss_fn = load_loss(args.loss.loss, args.loss)
+
+        encoder = load_encoder(args.encoder).build(
+            dataset.seq_length*dataset.seq_dim, latent_dataset.dim)
+        encoder.to(device)
+
+        optimizer = load_optimizer(encoder.parameters(), args.optimizer)
+
+        torch.manual_seed(1234)
+
+        logger = Logger(args.save_dir)
+        logger.save_args(args)
+
+        loss_mean = 0
+        n_samples = 0
+        for epoch in range(args.num_epochs):
+            for _, biodata, index in tqdm(dataloader, disable=args.debug):
+                optimizer.zero_grad()
+
+                z = latent_dataset[index]
+                z = z.to(device)
+                biodata = biodata.to(device)
+
+                z_recons = encoder(biodata)
+                loss = loss_fn(z, z_recons).mean()
+                loss.backward()
+
+                optimizer.step(loss=loss)
+
+                loss_mean += loss.detach().item()*len(index)
+                n_samples += len(index)
+                if args.debug:
+                    print(loss_mean/n_samples)
+
+            loss_mean /= n_samples
+            print("Epoch %i / %i, Loss: %2f" % (epoch, args.num_epochs, loss_mean))
+            if args.debug:
+                break
+
+            logger.add({"loss": loss_mean})
+            logger.save_model("{epoch:04d}", encoder)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_arguments(Params, dest="params")
+    parser.add_arguments(ExecutorConfig, dest="executor")
+    args = parser.parse_args()
+
+    executor = load_executor(args.executor)
+    executor(TrainEncoder(), args.params)
