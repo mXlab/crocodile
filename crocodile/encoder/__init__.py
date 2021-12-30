c3d2f6265358509a5f7720ddc747c4c74c2c05a7
Hugo Berard <berard.hugo@gmail.com>
2021-12-30 16:38:07 +0100

    adding regnet to encoder list

--- crocodile/encoder/__init__.py
+++ crocodile/encoder/__init__.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from .efficientnet import EfficientNetOptions, EfficientNet
+from .regnet import RegNetOptions, RegNet
 from .mlp import MLP, MLPParams
 from .vgg import VGG, VGGOptions
 from dataclasses import dataclass
@@ -10,6 +12,8 @@ from simple_parsing.helpers.serialization import encode, register_decoding_fn
 class EncoderType(Enum):
     MLP = "mlp"
     VGG = "vgg"
+    EFFICIENTNET = "efficientnet"
+    REGNET = "regnet"
 
 
 @dataclass
@@ -17,6 +21,8 @@ class EncoderParams(Serializable):
     encoder: EncoderType = EncoderType.MLP
     mlp_options: MLPParams = MLPParams()
     vgg_options: VGGOptions = VGGOptions()
+    regnet_options: RegNetOptions = RegNetOptions()
+    efficientnet_options: EfficientNetOptions = EfficientNetOptions()
 
 
 def load_encoder(params: EncoderParams) -> Encoder:
@@ -24,6 +30,10 @@ def load_encoder(params: EncoderParams) -> Encoder:
         return MLP(params.mlp_options)
     elif params.encoder == EncoderType.VGG:
         return VGG(params.vgg_options)
+    elif params.encoder == EncoderType.REGNET:
+        return RegNet(params.regnet_options)
+    elif params.encoder == EncoderType.EFFICIENTNET:
+        return EfficientNet(params.regnet_options)
 
 
 @encode.register
