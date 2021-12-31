08b7d672c4c99b3fc0b47ffafcb81ef84152c379
Hugo Berard <berard.hugo@gmail.com>
2021-12-31 15:34:31 +0100

    Fixing bugs

--- crocodile/params.py
+++ crocodile/params.py
@@ -60,4 +60,28 @@ class TrainEncoderLatentParams(Serializable):
             if self.optimizer.optimizer == OptimizerType.SGD:
                 self.optimizer.lr = 20
             elif self.optimizer.optimizer == OptimizerType.ADAM:
+                self.optimizer.lr = 5e-4
+
+@dataclass
+class TrainEncoderParams(Serializable):
+    generator_path: Optional[Path] = None
+    epoch: Optional[int] = None
+    encoder: EncoderParams = EncoderParams()
+    dataset: LaurenceDataset.Params = LaurenceDataset.Params()
+    batch_size: int = 64
+    optimizer: OptimizerArgs = OptimizerArgs(momentum=0.9)
+    loss: LossParams = LossParams()
+    num_epochs: int = 100
+    log_dir: Path = Path("./results/encoder")
+    name: str = "test_1"
+    slurm_job_id: Optional[str] = None
+    debug: bool = False
+    num_test_samples: int = 8
+
+    def __post_init__(self):
+        self.save_dir = self.log_dir / self.name
+        if self.optimizer.lr is None:
+            if self.optimizer.optimizer == OptimizerType.SGD:
+                self.optimizer.lr = 20
+            elif self.optimizer.optimizer == OptimizerType.ADAM:
                 self.optimizer.lr = 2e-2
