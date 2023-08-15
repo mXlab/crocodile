03917480fafab69444003800f90250d1f59c1bc0
Hugo Berard <berard.hugo@gmail.com>
2023-08-15 19:55:39 -0400

    Adding mlflow

--- FastGAN/train.py
+++ FastGAN/train.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+"""FastGAN train module."""
+from dataclasses import dataclass, asdict
 import random
 from pathlib import Path
 from typing import Optional
@@ -10,8 +11,11 @@ from torch.utils.data.dataloader import DataLoader
 from torchvision import transforms
 from torchvision import utils as vutils
 from tqdm import tqdm
+import mlflow
 
 from crocodile.optimizer import AdamConfig, load_optimizer
+from crocodile.trainer.base import TrainConfig, Trainer
+from crocodile.utils import flatten_dict
 
 
 from . import lpips
@@ -26,13 +30,11 @@ from .operation import copy_G_params, load_params, get_dir
 from .operation import ImageFolder, InfiniteSamplerWrapper
 from .diffaug import DiffAugment
 
-percept = lpips.PerceptualLoss(model="net-lin", net="vgg", use_gpu=True)
-
-
 # torch.backends.cudnn.benchmark = True
 
 
 def crop_image_by_part(image: torch.Tensor, part: int):
+    """Crop image by part"""
     hw = image.shape[2] // 2
     if part == 0:
         return image[:, :, :hw, :hw]
@@ -68,7 +70,9 @@ def train_d(net: nn.Module, data: torch.Tensor, label: str = "real"):
 
 
 @dataclass
-class FastGANTrainConfig:
+class FastGANTrainConfig(TrainConfig):
+    """FastGAN train config class."""
+
     data_root: Path = Path("../lmdbs/art_landscape_1k")
     total_iterations: int = 50000
     checkpoint: Optional[str] = None
@@ -78,138 +82,157 @@ class FastGANTrainConfig:
     discriminator: DiscriminatorConfig = DiscriminatorConfig()
     optimizer_G: AdamConfig = AdamConfig(lr=0.0002, betas=(0.5, 0.999))
     optimizer_D: AdamConfig = AdamConfig(lr=0.0002, betas=(0.5, 0.999))
-    use_cuda: bool = True
-    multi_gpu: bool = True
     dataloader_workers: int = 8
     img_save_interval: int = 1000
     model_save_interval: int = 5000
     num_test_samples: int = 8
     ema_beta: float = 0.001
     policy: str = "color,translation"
-    
+    remote_server_uri: str = "https://crocodile-gqhfy6c73a-uc.a.run.app/"
+    experiment_name: str = "fastgan"
 
 
-def train(config: FastGANTrainConfig):
-    saved_model_folder, saved_image_folder = get_dir(config)
+class FastGANTrainer(Trainer):
+    """FastGAN trainer class."""
 
-    device = torch.device("cpu")
-    if config.use_cuda:
-        device = torch.device("cuda:0")
+    def __init__(self, config: FastGANTrainConfig):
+        self.config = config
 
-    transform_list = [
-        transforms.Resize((int(config.im_size), int(config.im_size))),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-    ]
-    trans = transforms.Compose(transform_list)
+        transform_list = [
+            transforms.Resize((int(config.im_size), int(config.im_size))),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+        trans = transforms.Compose(transform_list)
 
-    dataset = ImageFolder(root=config.data_root, transform=trans)
+        dataset = ImageFolder(root=config.data_root, transform=trans)
 
-    dataloader = iter(
-        DataLoader(
-            dataset,
-            batch_size=config.batch_size,
-            shuffle=False,
-            sampler=InfiniteSamplerWrapper(dataset),
-            num_workers=config.dataloader_workers,
-            pin_memory=True,
-        )
-    )
-
-    # from model_s import Generator, Discriminator
-    netG = Generator(im_size=config.im_size, config=config.generator)
-    netG.apply(weights_init)
-
-    netD = Discriminator(im_size=config.im_size, config=config.discriminator)
-    netD.apply(weights_init)
-
-    netG.to(device)
-    netD.to(device)
-
-    avg_param_G = copy_G_params(netG)
-
-    fixed_noise = netG.noise(config.num_test_samples).to(device)
-
-    optimizerG = load_optimizer(netG.parameters(), config.optimizer_G)
-    optimizerD = load_optimizer(netD.parameters(), config.optimizer_D)
-
-    current_iteration = 0
-    if config.checkpoint is not None:
-        ckpt = torch.load(config.checkpoint)
-        netG.load_state_dict(ckpt["g"])
-        netD.load_state_dict(ckpt["d"])
-        avg_param_G = ckpt["g_ema"]
-        optimizerG.load_state_dict(ckpt["opt_g"])
-        optimizerD.load_state_dict(ckpt["opt_d"])
-        current_iteration = int(config.checkpoint.split("_")[-1].split(".")[0])
-        del ckpt
-
-    if config.multi_gpu:
-        netG = nn.DataParallel(netG.to(device))
-        netD = nn.DataParallel(netD.to(device))
-
-    for iteration in tqdm(range(current_iteration, config.total_iterations + 1)):
-        real_image = next(dataloader)
-        real_image = real_image.to(device)
-        current_batch_size = real_image.size(0)
-        noise = netG.noise(current_batch_size).to(device)
-
-        fake_images = netG(noise)
-
-        real_image = DiffAugment(real_image, policy=config.policy)
-        fake_images = [DiffAugment(fake, policy=config.policy) for fake in fake_images]
-
-        ## 2. train Discriminator
-        netD.zero_grad()
-
-        err_dr, rec_img_all, rec_img_small, rec_img_part = train_d(
-            netD, real_image, label="real"
-        )
-        train_d(netD, [fi.detach() for fi in fake_images], label="fake")
-        optimizerD.step()
-
-        ## 3. train Generator
-        netG.zero_grad()
-        pred_g = netD(fake_images, "fake")
-        err_g = -pred_g.mean()
-
-        err_g.backward()
-        optimizerG.step()
-
-        for p, avg_p in zip(netG.parameters(), avg_param_G):
-            avg_p.mul_(1-config.ema_beta).add_(config.ema_beta * p.data)
-
-        if iteration % 100 == 0:
-            print("GAN: loss d: %.5f    loss g: %.5f" % (err_dr, -err_g.item()))
-
-        if iteration % config.img_save_interval == 0:
-            backup_para = copy_G_params(netG)
-            load_params(netG, avg_param_G)
-            with torch.no_grad():
-                vutils.save_image(
-                    netG(fixed_noise)[0].add(1).mul(0.5),
-                    saved_image_folder + "/%d.jpg" % iteration,
-                    nrow=4,
-                )
-            load_params(netG, backup_para)
-
-        if (
-            iteration % config.model_save_interval == 0
-            or iteration == config.total_iterations
-        ):
-            torch.save(
-                {
-                    "g": netG.state_dict(),
-                    "g_ema": avg_param_G,
-                    "d": netD.state_dict(),
-                    "config": config,
-                },
-                saved_model_folder + "/%.6d.pth" % iteration,
+        self.dataloader = iter(
+            DataLoader(
+                dataset,
+                batch_size=config.batch_size,
+                shuffle=False,
+                sampler=InfiniteSamplerWrapper(dataset),
+                num_workers=config.dataloader_workers,
+                pin_memory=True,
             )
+        )
+
+        # from model_s import Generator, Discriminator
+        self.netG = Generator(im_size=config.im_size, config=config.generator)
+        self.netG.apply(weights_init)
+
+        self.netD = Discriminator(im_size=config.im_size, config=config.discriminator)
+        self.netD.apply(weights_init)
+
+        self.optimizerG = load_optimizer(self.netG.parameters(), config.optimizer_G)
+        self.optimizerD = load_optimizer(self.netD.parameters(), config.optimizer_D)
+
+        self.percept = lpips.PerceptualLoss(model="net-lin", net="vgg", use_gpu=True)
+
+    def train(self):
+        mlflow.set_tracking_uri(self.config.remote_server_uri)
+        mlflow.set_experiment(experiment_name)
+        with mlflow.start_run():
+            params = flatten_dict(asdict(self.config))
+            print(params)
+            exit()
+
+            device = (
+                torch.device("cuda")
+                if torch.cuda.is_available()
+                else torch.device("cpu")
+            )
+
+            self.netG.to(device)
+            self.netD.to(device)
+
+            avg_param_G = copy_G_params(self.netG)
+            fixed_noise = self.netG.noise(self.config.num_test_samples).to(device)
+
+            current_iteration = 0
+            if self.config.checkpoint is not None:
+                ckpt = torch.load(self.config.checkpoint)
+                self.netG.load_state_dict(ckpt["g"])
+                self.netD.load_state_dict(ckpt["d"])
+                avg_param_G = ckpt["g_ema"]
+                self.optimizerG.load_state_dict(ckpt["opt_g"])
+                self.optimizerD.load_state_dict(ckpt["opt_d"])
+                current_iteration = int(
+                    self.config.checkpoint.split("_")[-1].split(".")[0]
+                )
+                del ckpt
+
+            saved_model_folder, saved_image_folder = get_dir(config)
+
+            for iteration in tqdm(
+                range(current_iteration, self.config.total_iterations + 1)
+            ):
+                real_image = next(self.dataloader)
+                real_image = real_image.to(device)
+                current_batch_size = real_image.size(0)
+                noise = self.netG.noise(current_batch_size).to(device)
+
+                fake_images = self.netG(noise)
+
+                real_image = DiffAugment(real_image, policy=self.config.policy)
+                fake_images = [
+                    DiffAugment(fake, policy=self.config.policy) for fake in fake_images
+                ]
+
+                ## 2. train Discriminator
+                self.netD.zero_grad()
+
+                err_dr, rec_img_all, rec_img_small, rec_img_part = train_d(
+                    self.netD, real_image, label="real"
+                )
+                train_d(self.netD, [fi.detach() for fi in fake_images], label="fake")
+                self.optimizerD.step()
+
+                ## 3. train Generator
+                self.netG.zero_grad()
+                pred_g = self.netD(fake_images, "fake")
+                err_g = -pred_g.mean()
+
+                err_g.backward()
+                self.optimizerG.step()
+
+                for p, avg_p in zip(self.netG.parameters(), avg_param_G):
+                    avg_p.mul_(1 - self.config.ema_beta).add_(
+                        self.config.ema_beta * p.data
+                    )
+
+                if iteration % 100 == 0:
+                    print("GAN: loss d: %.5f    loss g: %.5f" % (err_dr, -err_g.item()))
+
+                if iteration % self.config.img_save_interval == 0:
+                    backup_para = copy_G_params(self.netG)
+                    load_params(self.netG, avg_param_G)
+                    with torch.no_grad():
+                        vutils.save_image(
+                            self.netG(fixed_noise)[0].add(1).mul(0.5),
+                            saved_image_folder + "/%d.jpg" % iteration,
+                            nrow=4,
+                        )
+                    load_params(self.netG, backup_para)
+
+                if (
+                    iteration % self.config.model_save_interval == 0
+                    or iteration == self.config.total_iterations
+                ):
+                    torch.save(
+                        {
+                            "g": self.netG.state_dict(),
+                            "g_ema": avg_param_G,
+                            "d": self.netD.state_dict(),
+                            "config": self.config,
+                        },
+                        saved_model_folder + "/%.6d.pth" % iteration,
+                    )
 
 
 if __name__ == "__main__":
     config = parse(FastGANTrainConfig)
-
-    train(config)
+    trainer = FastGANTrainer(config)
+    trainer.train()
---
+++ crocodile/logger.py
@@ -0,0 +1,6 @@
+import mlflow
+
+
+class MLFlowLogger:
+    def __init__(self, remote_server_uri, experiment_name):
+        
