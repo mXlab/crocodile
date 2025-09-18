6fe7992bb923c5a099b580b3da9eae77e58fe291
Sofian Audry <info@sofianaudry.com>
2025-09-18 17:53:36 -0400

    Added control module to interoperate with Wekinator.

---
+++ open-stage-control/EmotionVectorsCustomModule.js
@@ -0,0 +1,39 @@
+// keep track of connected clients
+
+var clients = []
+
+app.on('open', (data, client)=>{
+    if (!clients.includes(client.id)) clients.push(client.id)
+})
+
+app.on('close', (data, client)=>{
+    if (clients.includes(client.id)) clients.splice(clients.indexOf(client.id))
+})
+
+module.exports = {
+
+    oscInFilter:function(data){
+
+        var {address, args, host, port} = data
+
+        // split
+        if (address === '/wek/output') {
+            for (let i=0; i<args.length; i++) {
+	            receive("/SET", "matrix_eigens/" + i, args[i], {clientId: clients[0]})
+            }
+
+            return // bypass original message
+        }
+
+        return {address, args, host, port}
+
+    },
+
+    oscOutFilter:function(data){
+
+        var {address, args, host, port, clientId} = data
+
+        return {address, args, host, port}
+    }
+
+}
